08b73dcdcbd4dca8cbf6f9aafaad557da2aba1d5
Maaxboon <maaxwelokoth@gmail.com>
2025-03-07 15:35:27 +0300

    Removes deleted files

--- frontend/business/script.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-  $("div").on("click", function () {
-    $(this).toggleClass("show-description");
-  });
-});
--- js-crash-course/operators.js
+++
@@ -1,2 +0,0 @@
-// Addition
-console.log(4 + 4 ) // 8
